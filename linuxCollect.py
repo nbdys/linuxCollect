3e33a22e9ea0f1f9b92d306a5326d650ee70919b
ssc <ssc@scisc.de>
2016-01-22 20:05:14 +0100

    Commands are not longer stored in the script.  Now a textfile with commands could be passed to the script.

--- linuxCollect.py
+++ linuxCollect.py
@@ -1,43 +1,60 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import fabric
 import argparse
 from fabric.api import *
+from fabric.exceptions import NetworkError
 
 # Liste der SSH-Hosts initialisieren
-rechner=[]
-
+rechner = []
+# ToDo: Mit Schalter aufteilen nach Grundbefehlen und erweiterteten Befehlen
 # Liste der Befehle
-befehle = ['hostname', 'date', 'uname -a', 'ls -l']
+commands = []
+
+def readCommands(c):
+    datei = open(str(c), 'rt')
+    for line in datei:
+        commands.append(line.strip())
+    datei.close()
+
+def readTarget(t):
+    for target in t:
+        rechner.append(target)
+    print t
+
+## ToDo: Abfangen, wenn die letzte Zeile der Liste ein Enter am Ende hat
+def readHostlist(h):
+    datei = open(str(h), 'rt')
+    for line in datei:
+        rechner.append(line.rstrip())
+    datei.close()
+
 
 # Parser für Argumente erstellen
-parser = argparse.ArgumentParser(description='Sammelt Informationen von Linuxsystemen per SSH', epilog="So zerbroeselt der Keks nunmal...", prog='Linux Collector')
+parser = argparse.ArgumentParser(description='Sammelt Informationen von Linuxsystemen per SSH',
+                                 epilog="So zerbroeselt der Keks nunmal...", prog='Linux Collector')
 
-parser.add_argument('-H', dest='hostlist', action="store", help='Pfad zur Hostliste, Format: user@host:Port')
-parser.add_argument('target', action="store", help='user@hostname:Port oder user@IP:Port', nargs='?')
-parser.add_argument('--version', action='version', version='%(prog)s 0.9beta')
+parser.add_argument('-d', '--datei', dest='hostlist', action="store", help='Pfad zur Hostliste, Format: user@host:Port')
+parser.add_argument('-t', '--target', dest='target', action="store",
+                    help='user@hostname:Port oder user@IP:Port, mehrere moeglich', nargs='*')
+parser.add_argument('-c', '--commands', dest='commands', action='store', default='default.txt', )
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.9beta')
 
 args = parser.parse_args()
 argsdict = vars(args)
 
 # Hostliste mit einzelnem Ziel befüllen
 if (args.target):
-    rechner = [ argsdict['target']]
+    readTarget(argsdict['target'])
 
 # Hostliste aus Datei befüllen
 elif (args.hostlist):
-    datei = open(str([argsdict['hostlist']]), 'rt')
-    for line in datei:
-        rechner.append(line.rstrip())
-    datei.close()
+    readHostlist(argsdict['hostlist'])
 
 # Wenn keine Argumente übergeben werden die Hilfe aufrufen
-else: parser.parse_args(['--help'])
+else:
+    parser.parse_args(['--help'])
 
-
-# Hostfile einlesen und in fabfile.py schreiben
+# Hostfile einlesen und in fabfile.py schreiben TODO: Schalter implementieren
 """
 def writeFabfile(hostlist, orderlist):
     fabfile = open('fabfile.py','a')
@@ -56,24 +73,34 @@ def writeFabfile(hostlist, orderlist):
 
     fabfile.close()
 
-writeFabfile(hosts,befehle)
+writeFabfile(hosts,commands)
 """
 
 # Befehle für jeden Host abarbeiten und Ausgaben in .html Datei schreiben
-if (args.target) or (args.hostliste):
+if (args.target) or (args.hostlist):
+    print rechner
     for h in rechner:
-      env.host_string = h
+        env.host_string = h  # Host zu dem die Verbindung aufgebaut wird
+        env.warn_only = True  # Nur Warnungen zeigen, nicht das Programm abbrechen
+        env.skip_bad_hosts = True  # Hosts überspringen, welche nicht erreichbar sind
+        env.timeout = '60'
+        print h
+        log = open(h + '.html', 'a')  # Logdatei erstellen und oeffnen
+        log.write(' <!-- HTML 4.x --> <meta http-equiv="content-type" content="text/html; charset=utf-8"> <!-- HTML5 --> <meta charset="utf-8">')
+        for b in commands:
+            try:
+                result = sudo(b)
+                if result.return_code != 0:
+                    continue
+                else:
+                    text = "<pre>" +result + "</pre>"
+                    log.write("<h3>" + b + "</h3>" + '\n')
+                    log.write(text.strip('\n'))
+                    log.write('\n\n')
+            except NetworkError as e:  # SSH Connection Refused abfangen
+                print e
+                break
 
-      log = open(h+'.html','a')
-
-      for b in befehle:
-          text ="<pre>"+ sudo(b)+"</pre>"
-          log.write("<h3>"+b+ "</h3>"+'\n')
-          log.write(text.strip('\n'))
-          log.write('\n\n')
-      log.close()
-
-
-
+        log.close()
 
 exit()
